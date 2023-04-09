f529fbda444ec6c8d73081be74f6eb3e6b35b7e7
Kavya Pasangulapati <kp00253@my.westga.edu>
2023-04-08 21:55:20 -0400

    Added Patient Registration form

---
+++ HospitalManagement/View/PatientRegistration.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace HospitalManagement.View
+{
+    partial class PatientRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuUserControl1 = new HospitalManagement.UserControls.MenuUserControl();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.registerPatientButton = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.zipCodeTextBox = new System.Windows.Forms.TextBox();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.streetAddressTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.birthDayDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // menuUserControl1
+            // 
+            this.menuUserControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.menuUserControl1.Location = new System.Drawing.Point(0, 0);
+            this.menuUserControl1.Name = "menuUserControl1";
+            this.menuUserControl1.Size = new System.Drawing.Size(814, 118);
+            this.menuUserControl1.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(59, 100);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(134, 32);
+            this.label.TabIndex = 1;
+            this.label.Text = "First Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Last Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date of Birth:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 577);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "State:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 493);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "City:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 415);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 32);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Street:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 655);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 32);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Zipcode:";
+            // 
+            // registerPatientButton
+            // 
+            this.registerPatientButton.Location = new System.Drawing.Point(325, 740);
+            this.registerPatientButton.Name = "registerPatientButton";
+            this.registerPatientButton.Size = new System.Drawing.Size(150, 46);
+            this.registerPatientButton.TabIndex = 9;
+            this.registerPatientButton.Text = "Register";
+            this.registerPatientButton.UseVisualStyleBackColor = true;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(340, 817);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(116, 32);
+            this.errorLabel.TabIndex = 10;
+            this.errorLabel.Text = "errorlabel";
+            this.errorLabel.Visible = false;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(311, 97);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(328, 39);
+            this.firstNameTextBox.TabIndex = 11;
+            // 
+            // zipCodeTextBox
+            // 
+            this.zipCodeTextBox.Location = new System.Drawing.Point(311, 652);
+            this.zipCodeTextBox.Name = "zipCodeTextBox";
+            this.zipCodeTextBox.Size = new System.Drawing.Size(328, 39);
+            this.zipCodeTextBox.TabIndex = 12;
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(311, 574);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(328, 39);
+            this.stateTextBox.TabIndex = 13;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(311, 490);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(328, 39);
+            this.cityTextBox.TabIndex = 14;
+            // 
+            // streetAddressTextBox
+            // 
+            this.streetAddressTextBox.Location = new System.Drawing.Point(311, 412);
+            this.streetAddressTextBox.Name = "streetAddressTextBox";
+            this.streetAddressTextBox.Size = new System.Drawing.Size(328, 39);
+            this.streetAddressTextBox.TabIndex = 15;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(311, 334);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(328, 39);
+            this.phoneNumberTextBox.TabIndex = 16;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(311, 174);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(328, 39);
+            this.lastNameTextBox.TabIndex = 17;
+            // 
+            // birthDayDateTimePicker
+            // 
+            this.birthDayDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.birthDayDateTimePicker.Location = new System.Drawing.Point(311, 257);
+            this.birthDayDateTimePicker.MaxDate = new System.DateTime(2023, 4, 8, 0, 0, 0, 0);
+            this.birthDayDateTimePicker.Name = "birthDayDateTimePicker";
+            this.birthDayDateTimePicker.Size = new System.Drawing.Size(328, 39);
+            this.birthDayDateTimePicker.TabIndex = 18;
+            this.birthDayDateTimePicker.Value = new System.DateTime(2023, 4, 8, 0, 0, 0, 0);
+            // 
+            // PatientRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 914);
+            this.Controls.Add(this.birthDayDateTimePicker);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.streetAddressTextBox);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.stateTextBox);
+            this.Controls.Add(this.zipCodeTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.registerPatientButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.menuUserControl1);
+            this.Name = "PatientRegistration";
+            this.Text = "Patient Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private UserControls.MenuUserControl menuUserControl1;
+        private Label label;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button registerPatientButton;
+        private Label errorLabel;
+        private TextBox firstNameTextBox;
+        private TextBox zipCodeTextBox;
+        private TextBox stateTextBox;
+        private TextBox cityTextBox;
+        private TextBox streetAddressTextBox;
+        private TextBox phoneNumberTextBox;
+        private TextBox lastNameTextBox;
+        private DateTimePicker birthDayDateTimePicker;
+    }
+}
