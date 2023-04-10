d2bc0cbda33c207e7c3faae8e26a57a673dd0de0
Kavya Pasangulapati <kp00253@my.westga.edu>
2023-04-10 16:49:58 -0400

    Added code to display appropriate labels and fields when  radio buttons are selected

--- HospitalManagement/View/SearchPatientBy.cs
+++ HospitalManagement/View/SearchPatientBy.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace HospitalManagement.View
 {
-    public partial class SearchPatient : Form
+    public partial class SearchPatientBy : Form
     {
-        public SearchPatient()
+        public SearchPatientBy()
         {
             InitializeComponent();
+        }
+
+        private void dobRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            dobLabel.Visible = true;
+            dobDateTimePicker.Visible = true;
+            lastnameLabel.Visible = false;
+            lastnameTextBox.Visible = false;
+            firstnameLabel.Visible = false;
+            firstnametextBox.Visible = false;
+        }
+
+        private void dobLnRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            dobLabel.Visible = true;
+            dobDateTimePicker.Visible = true;
+            lastnameLabel.Visible = true;
+            lastnameTextBox.Visible = true;
+            firstnameLabel.Visible = false;
+            firstnametextBox.Visible = false;
+        }
+
+        private void fnLnRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            dobLabel.Visible = false;
+            dobDateTimePicker.Visible = false;
+            lastnameLabel.Visible = true;
+            lastnameTextBox.Visible = true;
+            firstnameLabel.Visible = true;
+            firstnametextBox.Visible = true;
         }
     }
 }
